a2c99a1034b8b1050fcc13996924d881b4fef3af
jianyu-m <jianyu@hku.hk>
2017-11-29 02:22:15 +0800

    more

--- com.py
+++ com.py
@@ -9,10 +9,64 @@ def log_e(d, e):
     if d == 0:
         return 0
     else:
-        return - d * math.log2(d / e)
+        return - d * math.log(d / e)
 
 if __name__ == "__main__":
-    p = numpy.fromfile("parr")
+    arr = numpy.zeros((N, N))
+    with open("Graph.txt") as f:
+        line = " "
+        while len(line) > 0:
+            line = f.readline()
+            if len(line) <= 0:
+                break
+            linarr = line.split(" ")
+            fr, to = int(linarr[0]), int(linarr[1])
+            arr[fr - 1][to - 1] = 1
+            arr[to - 1][fr - 1] = 1
+
+#    N = 100
+
+    # arr = numpy.array([[0, 1, 0, 1, 1],
+    #                    [1, 0, 0, 0, 1],
+    #                    [0, 0, 0, 1, 1],
+    #                    [1, 0, 1, 0, 0],
+    #                    [1, 1, 1, 0, 0]])
+
+    A = numpy.zeros((N, N))
+
+    sum_all = [sum(arr[i]) for i in range(N)]
+
+    for i in range(N):
+        for j in range(N):
+            if arr[i][j] == 1:
+                A[i][j] = 1 / sum_all[j]
+            else:
+                A[i][j] = 0
+
+    e = numpy.zeros((N, N))
+    p = numpy.zeros((N, N))
+    for i in range(N):
+        for j in range(N):
+            if arr[i][j] == 1:
+                e[i][j] = 1 / sum_all[i]
+            else:
+                e[i][j] = 0
+
+    itr = 10
+
+    convergence = 10e-5
+    tmp = 100
+
+    print("start")
+    while tmp > convergence:
+        print("iter start")
+        p_now = (1 - alpha) * numpy.dot(A, p) + alpha * e
+        print("iter fin")
+        tmp = sum(sum((p_now - p)**2))
+        p = p_now
+        print(str(tmp))
+
+    p.dump("parr")
 
     points = kmeans(2).fit(p)
 
@@ -32,9 +86,9 @@ if __name__ == "__main__":
     class_1 = {"Clinton": 0, "Trump": 0}
     total = 0
     for idx, p in enumerate(points.labels_):
+        total += 1
         if idx not in labels:
             continue
-        total += 1
         if p == 0:
             if labels[idx] == 0:
                 class_0["Clinton"] += 1
@@ -57,6 +111,8 @@ if __name__ == "__main__":
         class_1_label = "Trump"
 
     purity = (class_0[class_0_label] + class_1[class_1_label]) / total
+
+
 
     # compute entropy
 
